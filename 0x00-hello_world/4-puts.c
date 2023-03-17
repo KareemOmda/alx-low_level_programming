758861b57106eac0014442b0b14f4adf9a86573a
Kareem <kareem.gahamy@gmail.com>
2023-03-16 19:09:16 -0700

    Your commit message

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A C program
+ *
+ * Ruturn: Always 0 (success)
+*/
+
+int main(void)
+{
+	puts("\""Programming is like building a multilingual puzzle");
+	return (0);
+}
