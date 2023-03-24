3b8a8194cf8f8f9b25eeff6aa56b4b92a04e9059
KareemOmda <74760780+KareemOmda@users.noreply.github.com>
2023-03-24 04:54:19 +0200

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,29 +1,14 @@
 #include "main.h"
-/**
- * mul - multiply 2 int
- * @a: first int
- * @b: second int
- * Return: addition of the multiplicattion
- */
-
-int mul(int a, int b)
-{
-	return (a * b);
-}
-
-
-2-main.c
-#include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* _isdigit - write a function that check for a digit (0 through 9).
+* @c: char to check
+* Return:  0 or 1
+**/
+int _isdigit(int c)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
