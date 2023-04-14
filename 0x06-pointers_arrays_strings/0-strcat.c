22184e8ed366df94fefaf738a12ca431625c9661
KareemOmda <74760780+KareemOmda@users.noreply.github.com>
2023-04-14 17:14:31 +0200

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,21 +6,14 @@
  * @src:char
  * Return:char
  */
-char *_strcat(char *dest, char *src)
+
+char *strcat(char *dest, const char *src) 
 {
-	char *s = dest;
-
-	while (*dest != '\0')
-	{
-		dest++;
-	}
-
-	while (*src != '\0')
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-	return (s);
+   char *ptr = dest + strlen(dest);
+   while (*src != '\0') 
+   {
+       *ptr++ = *src++;
+   }
+   *ptr = '\0';
+   return dest;
 }
