fb0534e88733d27ccbdc6a06ffb5a7e1e05c80d7
kareem <kareem.gahamy@gmail.com>
2023-04-29 04:13:34 +0200

    Your commit message

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,2 +1,22 @@
-:wq
-`
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - checks code for Holberton School students.
+ * @h: name of list
+ * Return: Number of nodes.
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h)
+i	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
