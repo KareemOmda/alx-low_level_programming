05ad83275aeeb604a82286f1fd0dbf5e66d1ba78
KareemOmda <74760780+KareemOmda@users.noreply.github.com>
2023-05-01 06:54:24 +0200

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -13,9 +13,9 @@ int str_check(char *s1, char *s2, int i, int j)
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
-		return (str_checker(s1, s2, i + 1, j + 1));
+		return (str_check(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (str_checker(s1, s2, i, j + 1));
+		return (str_check(s1, s2, i, j + 1));
 	if (s2[j] == '*')
 		return (str_check(s1, s2, i + 1, j) || str_check(s1, s2, i, j + 1));
 	return (0);
